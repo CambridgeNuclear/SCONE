692b68904af814625dd81630e4b1147219f1d29a
M. A. Kowalski <mak60@cam.ac.uk>
2019-02-22 19:37:47 +0000

    Introduced fortran intrinsic 32/64 bit ints into hash implementation.
    For unknown reason FNV-1 64bit implementation is not working. It was
    removed from compilation. This issue may be investigated at the latter
    date but is not essential.

--- MC Code/SharedModules/hashFunctions_func.f90
+++ MC Code/SharedModules/hashFunctions_func.f90
@@ -2,6 +2,7 @@ module hashFunctions_func
 
   use numPrecision
   use genericProcedures, only : fatalError
+  use iso_fortran_env,   only : int8,  int32, int64
 
   implicit none
   private
@@ -12,8 +13,8 @@ module hashFunctions_func
   !!
   public :: FNV_1
   interface FNV_1
-    module procedure FNV_1_shortInt
-    module procedure FNV_1_longInt
+    module procedure FNV_1_int32
+    !module procedure FNV_1_int64
   end interface
 
   !!
@@ -35,82 +36,79 @@ contains
   !! NOTE: Ratio was changed for fun -> no particular reason
   !!
   !!
-  pure function knuthHash(key,m) result(hash)
-    integer(shortInt), intent(in) :: key
-    integer(shortInt), intent(in) :: m
-    integer(shortInt)             :: hash
-    integer(shortInt)             :: m_loc
-    integer(shortInt),parameter   :: kin64 = selected_int_kind(18) ! Choose at least 60-bit integer
-    integer(kin64)                :: hash_loc
-    integer(kin64),parameter      :: prime = 3180339487_kin64
+  elemental function knuthHash(key,m) result(hash)
+    integer(int32), intent(in) :: key
+    integer(int32), intent(in) :: m
+    integer(int32)             :: hash
+    integer(int32)             :: m_loc
+    integer(int64)             :: hash_loc
+    integer(int64),parameter   :: prime = 3180339487_int64
+    integer(int64),parameter   :: mask = 4294967295_int64
 
-    ! Constrain m to range 1-32
+    ! Constrain m to range 1-31
     m_loc = max(1,m)
-    m_loc = min(32,m_loc)
+    m_loc = min(31,m_loc)
 
-    ! Calculate prime * key modulo word
-    hash_loc = modulo(prime * key, 4294967296_kin64)
+    ! Calculate prime * key.
+    hash_loc = iand( prime * key, mask)
 
     ! Keep m uppermost bits
     hash = transfer(shiftr(hash_loc,32-m),shortInt)
 
   end function knuthHash
 
-
   !!
+  !! Implementation of Fowler-Noll-Vo 1 hash function for 32 bit integer
+  !! Prime and offset taken from:
+  !!   http://www.isthe.com/chongo/tech/comp/fnv/index.html
   !!
-  !!
-  pure subroutine FNV_1_shortInt(key,hash)
-    character(*),intent(in)         :: key
-    integer(shortInt), intent(out)  :: hash
-    integer(shortInt),parameter     :: FNV_prime  = 16777619_shortInt
-    integer(shortInt),parameter     :: FNV_offset = transfer(z'811c9dc5',shortInt)
-    integer(shortInt)               :: bajt, i
-    character(100),parameter    :: Here ='FNV_1_shortInt (hashFunctions_func.f90)'
+  pure subroutine FNV_1_int32(key, hash)
+    character(*),intent(in)     :: key
+    integer(int32), intent(out) :: hash
+    integer(int32),parameter    :: FNV_prime  = 16777619_shortInt
+    integer(int32),parameter    :: FNV_offset = transfer(z'811c9dc5',int32)
+    integer(int32)              :: bajt
+    integer(shortInt)           :: i
 
-    !! Hash functions should be pure -> Change error checking
-   ! if(storage_size(hash) /= 32) call fatalError(Here,'hash int is not 32bit')
-
-    bajt = ichar(key(1:1),shortInt)
+    bajt = iachar(key(1:1), int32)
     hash = FNV_offset
     hash = hash * FNV_prime
     hash = ieor(hash,bajt)
 
     do i=2,len(key)
-      bajt = ichar(key(i:i),shortInt)
+      bajt = iachar(key(i:i), int32)
       hash = hash * FNV_prime
       hash = ieor(hash,bajt)
     end do
 
-  end subroutine FNV_1_shortInt
+  end subroutine FNV_1_int32
 
-  !!
-  !!
-  !!
-  subroutine FNV_1_longInt(key,hash)
-    character(*),intent(in)      :: key
-    integer(longInt),intent(out) :: hash
-    integer(longInt),parameter   :: FNV_prime  = 1099511628211 _longInt
-    integer(longInt),parameter   :: FNV_offset = transfer(z'cbf29ce484222325',longInt)
-    integer(longInt)             :: bajt
-    integer(shortInt)            :: i
-    character(100),parameter   :: Here ='FNV_1_longInt (hashFunctions_func.f90)'
-
-    !! Hash functions should be pure -> Change error checking
-    !if(storage_size(hash) /= 64) call fatalError(Here,'hash int is not 64bit')
-
-    bajt = ichar(key(1:1),shortInt)
-    hash = FNV_offset
-    hash = hash * FNV_prime
-    hash = ieor(hash,bajt)
-
-    do i=2,len(key)
-      bajt = ichar(key(i:i),shortInt)
-      hash = hash * FNV_prime
-      hash = ieor(hash,bajt)
-    end do
-
-  end subroutine FNV_1_longInt
+!  !!
+!  !! Implementation of Fowler-Noll-Vo 1 hash function for 64 bit integer
+!  !! Prime and offset taken from: 14695981039346656037
+!  !!   http://www.isthe.com/chongo/tech/comp/fnv/index.html
+!  !!
+!  subroutine FNV_1_int64(key, hash)
+!    character(*),intent(in)    :: key
+!    integer(int64),intent(out) :: hash
+!    integer(int64),parameter   :: FNV_prime  = 1099511628211_int64
+!    integer(int64),parameter   :: FNV_offset = transfer(z'cbf29ce484222325',int64)
+!    integer(int64)             :: bajt
+!    integer(shortInt)          :: i
+!
+!    bajt = ichar(key(1:1),int64)
+!    hash = FNV_offset
+!    hash = hash * FNV_prime
+!    hash = ieor(hash,bajt)
+!
+!    do i=2,len(key)
+!      bajt = ichar(key(i:i),int64)
+!      hash = hash * FNV_prime
+!      hash = ieor(hash, bajt)
+!
+!    end do
+!
+!  end subroutine FNV_1_int64
 
     
 end module hashFunctions_func
