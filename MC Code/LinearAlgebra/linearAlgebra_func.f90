aab0be7858ad40791bf1f096a2c608de43550b80
M. A. Kowalski <mak60@cam.ac.uk>
2019-03-08 14:40:53 +0000

    Changed compilation. Now works with parallel make (-j flag)

---
+++ MC Code/LinearAlgebra/linearAlgebra_func.f90
@@ -0,0 +1,18 @@
+!!
+!! This module is a frontend for a linear algebra libraries
+!! No libraries should be used by other modules directly. If an additional procedure is needed
+!! approperiate interface should be added here.
+!!
+module linearAlgebra_func
+
+  use numPrecision
+
+  implicit none
+  private
+
+
+
+contains
+
+
+end module linearAlgebra_func
