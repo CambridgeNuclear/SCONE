f3ef7ac281a6b5d243050c5375649dc77e6f0499
ajb343 <ajb343@cam.ac.uk>
2022-10-10 12:47:35 +0100

    Changed to a use either DT or ST depending on opacities

--- TransportOperator/transportOperatorIMC_class.f90
+++ TransportOperator/transportOperatorIMC_class.f90
@@ -33,9 +33,12 @@ module transportOperatorIMC_class
   !!
   type, public, extends(transportOperator) :: transportOperatorIMC
     class(mgIMCMaterial), pointer, public :: mat    => null()
+    real(defReal)                         :: majorant_inv
   contains
     procedure          :: transit => imcTracking
     procedure, private :: materialTransform
+    procedure, private :: surfaceTracking
+    procedure, private :: deltaTracking
   end type transportOperatorIMC
 
 contains
@@ -46,58 +49,55 @@ contains
     type(tallyAdmin), intent(inout)            :: tally
     class(particleDungeon), intent(inout)      :: thisCycle
     class(particleDungeon), intent(inout)      :: nextCycle
-    real(defReal)                              :: majorant_inv, sigmaT, dTime, dColl
+    real(defReal)                              :: sigmaT, dTime, dColl, dist
+    logical(defBool)                           :: finished
     character(100), parameter :: Here = 'IMCTracking (transportOperatorIMC_class.f90)' 
 
-    ! Get majornat XS inverse: 1/Sigma_majorant
-    majorant_inv = ONE / self % xsData % getMajorantXS(p)
+    finished = .false.
+
+    ! Get majorant XS inverse: 1/Sigma_majorant
+    self % majorant_inv = ONE / self % xsData % getMajorantXS(p)
+
+    ! Deal with material particles, only relevant for ISMC
+    if(p % getType() == P_MATERIAL_MG) then
+      call self % materialTransform(p, tally)
+      if(p % fate == TIME_FATE) return
+    end if
 
     IMCLoop:do
 
-      ! Deal with material particles, only relevant for ISMC
-      if(p % getType() == P_MATERIAL_MG) then
-        call self % materialTransform(p, tally)
-        if(p % fate == TIME_FATE) exit IMCLoop
-      end if
+      ! Check for errors
+      if (p % getType() /= P_PHOTON_MG) call fatalError(Here, 'Particle is not MG Photon')
+      if (p % time /= p % time) call fatalError(Here, 'Particle time is NaN')
 
-      if(p % getType() .ne. P_PHOTON_MG) call fatalError(Here, 'Particle is not MG Photon')
+      ! Obtain sigmaT
+      sigmaT = self % xsData % getTransMatXS(p, p % matIdx())
 
       ! Find distance to time boundary
       dTime = lightSpeed * (p % timeMax - p % time)
 
-      ! Sample distance to move particle before potential collision
-      dColl = -log( p % pRNG % get() ) * majorant_inv
+      ! Sample distance to move particle before collision
+      dColl = -log( p % pRNG % get() ) / sigmaT
 
-      ! Determine which distance to move particle
-      if (dColl < dTime) then
-        ! Move partice to potential collision location
-        call self % geom % teleport(p % coords, dColl)
-        p % time = p % time + dColl / lightSpeed
+      ! Decide whether to use delta tracking or surface tracking
+      ! Vastly different opacities make delta tracking infeasable
+      if(sigmaT * self % majorant_inv > 0.3) then
+        ! Delta tracking
+        call self % deltaTracking(p, dTime, dColl, finished)
       else
-        ! Move particle to end of time step location
-        call self % geom % teleport(p % coords, dTime)
-        p % fate = TIME_FATE
-        p % time = p % timeMax
+        ! Surface tracking
+        call self % surfaceTracking(p, dTime, dColl, finished)
       end if
 
-      ! If particle has leaked exit
+      ! Check for particle leakage
       if (p % matIdx() == OUTSIDE_FILL) then
         p % fate = LEAK_FATE
         p % isDead = .true.
-        return
+        exit IMCLoop
       end if
 
-      if (p % fate == TIME_FATE) exit IMCLoop
-
-      ! Check for void
-      if( p % matIdx() == VOID_MAT) cycle IMCLoop
-
-      ! Obtain the local cross-section
-      sigmaT = self % xsData % getTransMatXS(p, p % matIdx())
-
-      ! Roll RNG to determine if the collision is real or virtual
-      ! Exit the loop if the collision is real
-      if (p % pRNG % get() < sigmaT*majorant_inv) exit IMCLoop
+      ! Exit if transport is finished
+      if (finished .eqv. .true.) exit IMCLoop
 
     end do IMCLoop
 
@@ -119,7 +119,7 @@ contains
     real(defReal)                              :: sigmaT, fleck, eta, mu, phi
     real(defReal), dimension(3)                :: dir
     character(100), parameter                  :: Here = 'materialTransform (transportOperatorIMC_class.f90)'
- 
+
     ! Confirm that time = 0
     !if (p % time .ne. 0) call fatalError(Here, 'Material particle should have time = 0')
 
@@ -133,6 +133,9 @@ contains
 
     ! Sample time to transform into radiation photon
     p % time = p % time - log(p % pRNG % get()) / (sigmaT*fleck*eta*lightSpeed)
+
+    ! Deal with eta = 0
+    if (p % time /= p % time) p % time = INF
 
     ! Exit loop if particle remains material until end of time step
     if (p % time >= p % timeMax) then
@@ -153,5 +156,74 @@ contains
 
   end subroutine materialTransform
 
+  !!
+  !! Perform surface tracking
+  !!
+  subroutine surfaceTracking(self, p, dTime, dColl, finished)
+    class(transportOperatorIMC), intent(inout) :: self
+    class(particle), intent(inout)             :: p
+    real(defReal), intent(in)                  :: dTime
+    real(defReal), intent(in)                  :: dColl
+    logical(defBool), intent(inout)            :: finished
+    real(defReal)                              :: dist
+    integer(shortInt)                          :: event
+    character(100), parameter                  :: Here = 'surfaceTracking (transportOperatorIMC_class.f90)'
+
+    dist = min(dTime, dColl)
+
+    ! Move through geometry using minimum distance
+    call self % geom % move(p % coords, dist, event)
+
+    p % time = p % time + dist / lightSpeed
+
+    ! Check result of transport
+    if (dist == dTime) then
+      ! Time boundary
+      if (event /= COLL_EV) call fatalError(Here, 'Move outcome should be COLL_EV after moving dTime')
+      p % fate = TIME_FATE
+      if (p % time /= p % timeMax) call fatalError(Here, 'Particle time is somehow incorrect')
+      p % time = p % timeMax
+      finished = .true.
+    else if (dist == dColl) then
+      ! Collision, increase time accordingly
+      if (event /= COLL_EV) call fatalError(Here, 'Move outcome should be COLL_EV after moving dTime')
+      finished = .true.
+    end if
+
+  end subroutine surfaceTracking
+
+  !!
+  !! Perform delta tracking
+  !!
+  subroutine deltaTracking(self, p, dTime, dColl, finished)
+    class(transportOperatorIMC), intent(inout) :: self
+    class(particle), intent(inout)             :: p
+    real(defReal), intent(in)                  :: dTime
+    real(defReal), intent(in)                  :: dColl
+    logical(defBool), intent(inout)            :: finished
+    real(defReal)                              :: sigmaT
+
+    ! Determine which distance to move particle
+    if (dColl < dTime) then
+      ! Move partice to potential collision location
+      call self % geom % teleport(p % coords, dColl)
+      p % time = p % time + dColl / lightSpeed
+    else
+      ! Move particle to end of time step location
+      call self % geom % teleport(p % coords, dTime)
+      p % fate = TIME_FATE
+      p % time = p % timeMax
+      finished = .true.
+      return
+    end if
+
+    ! Obtain local cross-section
+    sigmaT = self % xsData % getTransMatXS(p, p % matIdx())
+
+    ! Roll RNG to determine if the collision is real or virtual
+    ! Exit the loop if the collision is real
+    if (p % pRNG % get() < sigmaT * self % majorant_inv) finished = .true.
+
+  end subroutine deltaTracking
 
 end module transportOperatorIMC_class
