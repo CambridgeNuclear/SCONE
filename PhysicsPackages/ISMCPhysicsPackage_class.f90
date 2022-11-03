5a223d945b9e7501dfcb86e1bc94177c4a433db7
ajb343 <ajb343@cam.ac.uk>
2022-11-03 13:54:56 +0000

    Changed cycles to steps and dungeons to pointers to be in line with IMCPhysicsPackage

--- PhysicsPackages/ISMCPhysicsPackage_class.f90
+++ PhysicsPackages/ISMCPhysicsPackage_class.f90
@@ -73,7 +73,7 @@ module ISMCPhysicsPackage_class
     type(tallyAdmin),pointer               :: imcWeightAtch => null()
 
     ! Settings
-    integer(shortInt)  :: N_cycles
+    integer(shortInt)  :: N_steps
     integer(shortInt)  :: pop
     integer(shortInt)  :: limit
     real(defReal)      :: deltaT
@@ -86,8 +86,9 @@ module ISMCPhysicsPackage_class
     integer(shortInt)  :: nMat
 
     ! Calculation components
-    type(particleDungeon), allocatable :: thisCycle
-    type(particleDungeon), allocatable :: nextCycle
+    type(particleDungeon), pointer :: thisStep     => null()
+    type(particleDungeon), pointer :: nextStep     => null()
+    type(particleDungeon), pointer :: temp_dungeon => null()
     type(particleDungeon), allocatable :: matPhotons
       ! Note that other physics packages used pointers for these particleDungeons ( => null() )
       ! I found it easier to get 'allocatable' to work, unsure if this needs to be changed
@@ -102,7 +103,7 @@ module ISMCPhysicsPackage_class
   contains
     procedure :: init
     procedure :: printSettings
-    procedure :: cycles
+    procedure :: steps
     procedure :: collectResults
     procedure :: run
     procedure :: kill
@@ -117,7 +118,7 @@ contains
     print *, repeat("<>",50)
     print *, "/\/\ ISMC CALCULATION /\/\"
 
-    call self % cycles(self % tally, self % imcWeightAtch, self % N_cycles)
+    call self % steps(self % tally, self % imcWeightAtch, self % N_steps)
     call self % collectResults()
 
     print *
@@ -126,13 +127,13 @@ contains
   end subroutine
 
   !!
-  !! Run cycles for calculation
+  !! Run steps for calculation
   !!
-  subroutine cycles(self, tally, tallyAtch, N_cycles)
+  subroutine steps(self, tally, tallyAtch, N_steps)
     class(ISMCPhysicsPackage), intent(inout)         :: self
     type(tallyAdmin), pointer,intent(inout)         :: tally
     type(tallyAdmin), pointer,intent(inout)         :: tallyAtch
-    integer(shortInt), intent(in)                   :: N_cycles
+    integer(shortInt), intent(in)                   :: N_steps
     integer(shortInt)                               :: i, j, matIdx
     integer(shortInt), dimension(:), allocatable    :: Nm, Np
     type(particle)                                  :: p
@@ -140,7 +141,7 @@ contains
     real(defReal), dimension(:), allocatable        :: tallyEnergy
     class(IMCMaterial), pointer                     :: mat
     logical(defBool)                                :: printUpdates
-    character(100),parameter :: Here ='cycles (ISMCPhysicsPackage_class.f90)'
+    character(100),parameter :: Here ='steps (ISMCPhysicsPackage_class.f90)'
     class(tallyResult), allocatable                 :: tallyRes
 
     ! Set whether or not to print energy and temperature updates of each material
@@ -164,7 +165,7 @@ contains
     allocate(tallyEnergy(self % nMat))
 
     ! Generate initial material photons
-    call self % ISMCSource % generate(self % nextCycle, self % pop, p % pRNG)
+    call self % ISMCSource % generate(self % nextStep, self % pop, p % pRNG)
 
     open(unit = 10, file = 'temps.txt')
     open(unit = 11, file = 'pops.txt')
@@ -175,43 +176,45 @@ contains
     ! Build connections between materials
     call self % transOp % buildMajMap(p % pRNG, self % nucData)
 
-    do i=1,N_cycles
+    do i=1,N_steps
 
       write(10, '(8A)') numToChar(i)
 
       Nm = 0
       Np = 0
 
-      ! Store photons remaining from previous cycle
-      self % thisCycle = self % nextCycle
-      call self % nextCycle % cleanPop()
+      ! Swap dungeons to store photons remaining from previous time step
+      self % temp_dungeon => self % nextStep
+      self % nextStep     => self % thisStep
+      self % thisStep     => self % temp_dungeon
+      call self % nextStep % cleanPop()
 
       ! Generate from input source
       if( self % sourceGiven ) then
 
         ! Reduce size of dungeon if dungeon will overflow
-        !if( self % thisCycle % popSize() + self % pop > self % limit) then
-        !  call self % thisCycle % reduceSize2(self % limit - self % pop, self % nMat, self % geom, p % pRNG)
+        !if( self % thisStep % popSize() + self % pop > self % limit) then
+        !  call self % thisStep % reduceSize2(self % limit - self % pop, self % nMat, self % geom, p % pRNG)
         !end if
 
-        !call self % thisCycle % reduceSize2(self % limit, self % nMat, self % geom, p % pRNG)
+        !call self % thisStep % reduceSize2(self % limit, self % nMat, self % geom, p % pRNG)
 
-        call self % inputSource % append(self % thisCycle, self % pop, p % pRNG)
+        call self % inputSource % append(self % thisStep, self % pop, p % pRNG)
 
       end if
 
       !if(self % printSource == 1) then
-      !  call self % thisCycle % printToFile(trim(self % outputFile)//'_source'//numToChar(i))
+      !  call self % thisStep % printToFile(trim(self % outputFile)//'_source'//numToChar(i))
       !end if
 
-      call tally % reportCycleStart(self % thisCycle)
+      call tally % reportCycleStart(self % thisStep)
 
       ! Update majorants for transport operator
       call self % transOp % updateMajorants(p % pRNG)
 
       gen: do
         ! Obtain paticle from dungeon
-        call self % thisCycle % release(p)
+        call self % thisStep % release(p)
         call self % geom % placeCoord(p % coords)
 
         ! Assign particle time
@@ -229,7 +232,7 @@ contains
           ! Transport particle until its death
           history: do
 
-            call self % transOp % transport(p, tally, self % thisCycle, self % nextCycle)
+            call self % transOp % transport(p, tally, self % thisStep, self % nextStep)
             if(p % fate == LEAK_FATE) exit history
             
             if(p % fate == TIME_FATE) then
@@ -244,7 +247,7 @@ contains
                 end if
                 ! Store particle for use in next time step
                 p % fate = 0
-                call self % nextCycle % detain(p)
+                call self % nextStep % detain(p)
                 exit history
             end if
 
@@ -252,7 +255,7 @@ contains
               call fatalError(Here, 'Material particle should not undergo collision')
             end if
 
-            call self % collOp % collide(p, tally, self % thisCycle, self % nextCycle)
+            call self % collOp % collide(p, tally, self % thisStep, self % nextStep)
 
             if(p % isDead) call fatalError(Here, 'Particle should not be dead, check that collision &
                                                   &operator is of type "ISMCMGstd"')
@@ -260,29 +263,29 @@ contains
           end do history
 
         ! When dungeon is empty, exit
-        if( self % thisCycle % isEmpty() ) then
+        if( self % thisStep % isEmpty() ) then
           exit gen
         end if
 
       end do gen
 
       ! Send end of cycle report
-      call tally % reportCycleEnd(self % thisCycle)
+      call tally % reportCycleEnd(self % thisStep)
 
       ! Calculate times
       call timerStop(self % timerMain)
       elapsed_T = timerTime(self % timerMain)
 
       ! Predict time to end
-      end_T = real(N_cycles,defReal) * elapsed_T / i
+      end_T = real(N_steps,defReal) * elapsed_T / i
       T_toEnd = max(ZERO, end_T - elapsed_T)
 
       ! Display progress
       call printFishLineR(i)
       print *
       print *
-      print *, 'Source batch: ', numToChar(i), ' of ', numToChar(N_cycles)
-      print *, 'Pop:          ', numToChar(self % nextCycle % popSize())
+      print *, 'Source batch: ', numToChar(i), ' of ', numToChar(N_steps)
+      print *, 'Pop:          ', numToChar(self % nextStep % popSize())
       print *, 'Elapsed time: ', trim(secToChar(elapsed_T))
       print *, 'End time:     ', trim(secToChar(end_T))
       print *, 'Time to end:  ', trim(secToChar(T_toEnd))
@@ -314,7 +317,7 @@ contains
       ! Reset tally for next cycle
       call tallyAtch % reset('imcWeight')
 
-      print *, 'Completed: ', numToChar(i), ' of ', numToChar(N_cycles)
+      print *, 'Completed: ', numToChar(i), ' of ', numToChar(N_steps)
 
       write(11, '(8A)') 'M ', numToChar(Nm)
       write(11, '(8A)') 'P ', numToChar(Np)
@@ -324,7 +327,7 @@ contains
     close(10)
     close(11)
 
-  end subroutine cycles
+  end subroutine steps
 
   !!
   !! Print calculation results to file
@@ -343,7 +346,7 @@ contains
     call out % printValue(self % pop,name)
 
     name = 'Source_batches'
-    call out % printValue(self % N_cycles,name)
+    call out % printValue(self % N_steps,name)
 
     call cpu_time(self % CPU_time_end)
     name = 'Total_CPU_Time'
@@ -373,7 +376,7 @@ contains
     character(10)                                   :: time
     character(8)                                    :: date
     character(:),allocatable                        :: string
-    character(nameLen)                              :: nucData, energy, geomName
+    character(nameLen)                              :: nucData, geomName
     type(outputFile)                                :: test_out
     integer(shortInt)                               :: i
     character(nameLen), dimension(:), allocatable   :: mats
@@ -385,20 +388,10 @@ contains
     ! Read calculation settings
     call dict % get( self % pop,'pop')
     call dict % getOrDefault( self % limit, 'limit', self % pop)
-    call dict % get( self % N_cycles,'cycles')
+    call dict % get( self % N_steps,'steps')
     call dict % get( self % deltaT,'timeStepSize')
-    call dict % get( nucData, 'XSdata')
-    call dict % get( energy, 'dataType')
-
-    ! Process type of data
-    select case(energy)
-      case('mg')
-        self % particleType = P_PHOTON_MG
-      !case('ce')
-      !  self % particleType = P_PHOTON_CE
-      case default
-        call fatalError(Here,"dataType must be 'mg' or 'ce'.")
-    end select
+    self % particleType = P_PHOTON_MG
+    nucData = 'mg'
 
     ! Read outputfile path
     call dict % getOrDefault(self % outputFile,'outputFile','./output')
@@ -506,10 +499,10 @@ contains
     call self % tally % push(self % imcWeightAtch)
 
     ! Size particle dungeon
-    allocate(self % thisCycle)
-    call self % thisCycle % init(self % limit * self % nMat)
-    allocate(self % nextCycle)
-    call self % nextCycle % init(self % limit * self % nMat)
+    allocate(self % thisStep)
+    call self % thisStep % init(self % limit * self % nMat)
+    allocate(self % nextStep)
+    call self % nextStep % init(self % limit * self % nMat)
 
     call self % printSettings()
 
@@ -533,7 +526,7 @@ contains
 
     print *, repeat("<>",50)
     print *, "/\/\ ISMC CALCULATION /\/\"
-    print *, "Source batches:       ", numToChar(self % N_cycles)
+    print *, "Source batches:       ", numToChar(self % N_steps)
     print *, "Population per batch: ", numToChar(self % pop)
     print *, "Initial RNG Seed:     ", numToChar(self % pRNG % getSeed())
     print *
