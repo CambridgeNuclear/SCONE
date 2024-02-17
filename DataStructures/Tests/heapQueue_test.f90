4acb1400cf2a8b40f79a31c51ec94255a03576ed
Mikolaj-A-Kowalski <mikolaj.kowalski@cantab.net>
2024-02-17 19:03:21 +0100

    Add heap queue data structure
    It will be used for sampling without replacement.

---
+++ DataStructures/Tests/heapQueue_test.f90
@@ -0,0 +1,64 @@
+module heapQueue_test
+  use numPrecision
+  use heapQueue_class, only: heapQueue
+  use pFUnit_mod
+
+  implicit none
+
+contains
+
+!!<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+!! PROPER TESTS BEGIN HERE
+!!<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+  !!
+  !! Test with simple sequence without reaching maximum size
+  !!
+@Test
+  subroutine testBelowMaximum()
+    type(heapQueue)   :: hq
+    integer(shortInt) :: i
+    real(defReal), dimension(*), parameter :: seq = [2.0_defReal, 3.0_defReal, 1.0_defReal, 4.0_defReal, 5.0_defReal]
+
+    call hq % init(8)
+
+    @assertEqual(hq % getSize() , 0)
+
+    do i = 1, size(seq)
+      call hq % pushReplace(seq(i))
+    end do
+
+    ! Check that the maximum value is the maximum value in the sequence
+    @assertEqual(hq % maxValue() , maxval(seq))
+    @assertEqual(hq % getSize() , size(seq))
+
+  end subroutine testBelowMaximum
+
+  !!
+  !! Test the intended use case
+  !!
+  !! It is not a very good test but I had no better idea at the moment [MAK]
+  !!
+  @Test
+  subroutine testAboveMaximum()
+    type(heapQueue)   :: hq
+    integer(shortInt) :: i
+    real(defReal)     :: val
+    real(defReal), dimension(*), parameter :: seq = [2.0_defReal, 3.0_defReal, 1.0_defReal, 1.4_defReal, 5.0_defReal]
+
+    call hq % init(3)
+
+    ! Push upper bound
+    call hq % pushReplace(1000.0_defReal)
+
+    do i = 1, size(seq)
+      val = seq(i)
+      if (val < hq % maxValue()) call hq % pushReplace(seq(i))
+    end do
+
+    ! Check that the threshold is correct
+    @assertEqual(hq % maxValue() , 2.0_defReal)
+
+  end subroutine testAboveMaximum
+
+end module heapQueue_test
---
+++ DataStructures/heapQueue_class.f90
@@ -0,0 +1,183 @@
+module heapQueue_class
+  use numPrecision
+  use genericProcedures, only : swap
+  use errors_mod,        only : fatalError
+  implicit none
+  private
+
+  !!
+  !! A fixed size heap queue designed to store N smallest values of a sample
+  !!
+  !! The queue is implemented as a binary heap, that is a binary tree with the
+  !! heap property. The heap property is that the value of a parent node is
+  !! larger than the value of both its children. This means that the largest
+  !! value is always at the root of the tree.
+  !!
+  !! The queue is implemented as an array with the root at index 1. The children
+  !! of a node at index i are at 2*i and 2*i + 1. Note that for the even number
+  !! of elements the last node will only have one child.
+  !!
+  !! This data structure is intended to be used for sampling without replacement
+  !! as it allows to find a value of a threshold that selects K values from a
+  !! stream of the N random numbers.
+  !!
+  !! Interface:
+  !!   init        -> Initialise the queue to a given size
+  !!   pushReplace -> Add a value to the queue either growing the size or replacing the largest element
+  !!                  if maximum size was reached
+  !!   maxValue    -> Returns the largest value in the queue
+  !!   getSize     -> Returns the current size of the queue
+  !!
+  type, public :: heapQueue
+    private
+    real(defReal), dimension(:) , allocatable :: heap
+    integer(shortInt)                         :: size
+  contains
+    procedure :: init
+    procedure :: pushReplace
+    procedure :: maxValue
+    procedure :: getSize
+
+    procedure, private :: push
+    procedure, private :: replace
+  end type heapQueue
+
+contains
+
+  !!
+  !! Initialise the queue to a given size
+  !!
+  !! Args:
+  !!  maxSize [in] -> Maximum size of the queue
+  !!
+  subroutine init(self, maxSize)
+    class(heapQueue), intent(out) :: self
+    integer(shortInt), intent(in) :: maxSize
+
+    self % size = 0
+    allocate(self % heap(maxSize))
+
+  end subroutine init
+
+  !!
+  !! Add a value to to queue either growing the size or replacing the largest
+  !!
+  !! Args:
+  !!  value [in] -> Value to add to the queue
+  !!
+  subroutine pushReplace(self, val)
+    class(heapQueue), intent(inout) :: self
+    real(defReal), intent(in)       :: val
+
+    if (self % size < size(self % heap)) then
+      call self % push(val)
+    else
+      call self % replace(val)
+    end if
+
+  end subroutine pushReplace
+
+  !!
+  !! Add a value to the queue
+  !!
+  !! Assumes enough space is available
+  !!
+  !! Args:
+  !!  value [in] -> Value to add to the queue
+  !!
+  subroutine push(self, val)
+    class(heapQueue), intent(inout) :: self
+    real(defReal), intent(in)       :: val
+    integer(shortInt)               :: parent, child
+
+    ! Increase the size of the queue and add the new value
+    self % size = self % size + 1
+    self % heap(self % size) = val
+
+    ! Sift the new value up the heap to restore the heap property
+    child = self % size
+    parent = child / 2
+
+    do while (child > 1 .and. self % heap(parent) < self % heap(child))
+      call swap(self % heap(parent), self % heap(child))
+      child = parent
+      parent = child / 2
+    end do
+
+  end subroutine push
+
+  !!
+  !! Replaces the largest value in the queue with a new value
+  !!
+  !! Args:
+  !!  value [in] -> Value to add to the queue
+  !!
+  subroutine replace(self, val)
+    class(heapQueue), intent(inout) :: self
+    real(defReal), intent(in)       :: val
+    integer(shortInt)               :: parent, child
+
+    self % heap(1) = val
+
+    parent = 1
+    child = 2
+
+    ! Sift down the new value until heap property is restored be comparing
+    ! with the largest child and swapping if necessary
+    do while (child <= self % size)
+
+      ! We need to consider the case where there is only one child in a node
+      ! (can happen when the size is even). If child is the last element it is the
+      ! node with no sibling
+      if (child /= self % size .and. self % heap(child) < self % heap(child + 1)) then
+        child = child + 1
+      end if
+
+      ! If the parent is larger than the larger child we are done
+      if (self % heap(parent) >= self % heap(child)) then
+        return
+      end if
+
+      ! Otherwise swap the parent with the larger child and continue
+      ! the recursion
+      call swap(self % heap(parent), self % heap(child))
+      parent = child
+
+      ! Child points to the start of next level
+      child = parent * 2
+    end do
+
+  end subroutine replace
+
+  !!
+  !! Returns the largest value in the queue
+  !!
+  !! Errors:
+  !!   fatal error if the queue is empty
+  !!
+  function maxValue(self) result(val)
+    class(heapQueue), intent(in) :: self
+    real(defReal)                :: val
+    character(100), parameter :: Here = "maxValue (heapQueue_class.f90)"
+
+    if (self % size == 0) then
+      call fatalError(Here, "The queue is empty!")
+    end if
+
+    val = self % heap(1)
+
+  end function maxValue
+
+  !!
+  !! Get the current size of the queue
+  !!
+  pure function getSize(self) result(size)
+    class(heapQueue), intent(in) :: self
+    integer(shortInt)            :: size
+
+    size = self % size
+
+  end function getSize
+
+
+end module heapQueue_class
