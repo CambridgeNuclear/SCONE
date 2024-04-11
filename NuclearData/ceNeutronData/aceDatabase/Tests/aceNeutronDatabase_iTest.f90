0554c22740a086da256830ad18d52f84c7c49937
V. Raffuzzi(vr339) <vr339@eng.cam.ac.uk>
2024-04-11 16:23:14 +0100

    Fixing broken iTest

--- NuclearData/ceNeutronData/aceDatabase/Tests/aceNeutronDatabase_iTest.f90
+++ NuclearData/ceNeutronData/aceDatabase/Tests/aceNeutronDatabase_iTest.f90
@@ -75,7 +75,7 @@ contains
     ! Initialise data
     ptr => data
     call data % init(dataDict, ptr, silent = .true.)
-    call data % activate([1,2,3], silent = .true.)
+    call data % activate([1,2], silent = .true.)
 
     !!<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
     !! Perform tests
@@ -88,7 +88,7 @@ contains
     ! Get invalid materials
     @assertNotAssociated( ceNeutronMaterial_TptrCast( data % getMaterial(0)))
     @assertNotAssociated( ceNeutronMaterial_TptrCast( data % getMaterial(-4)))
-    @assertNotAssociated( ceNeutronMaterial_TptrCast( data % getMaterial(4)))
+    @assertNotAssociated( ceNeutronMaterial_TptrCast( data % getMaterial(3)))
 
     ! Get water
     mat => ceNeutronMaterial_TptrCast(data % getMaterial(1))
@@ -170,9 +170,6 @@ contains
     @assertTrue( 0 /= matNames % getOrDefault(name, 0))
 
     name = 'uo2'
-    @assertTrue( 0 /= matNames % getOrDefault(name, 0))
-
-    name = 'uo2hot'
     @assertTrue( 0 /= matNames % getOrDefault(name, 0))
 
     !<><><><><><><><><><><><><><><><><><><><><><><><>
