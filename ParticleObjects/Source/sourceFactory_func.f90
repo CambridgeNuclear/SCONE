c5fb2a22a75aeb84824cc0dfa1b2dc0935624a41
V. Raffuzzi <vr339@eng.cam.ac.uk>
2025-01-23 14:19:44 +0000

    Adding mixture of initial sources Update rng in PPs after source generation

--- ParticleObjects/Source/sourceFactory_func.f90
+++ ParticleObjects/Source/sourceFactory_func.f90
@@ -1,7 +1,7 @@
 module sourceFactory_func
 
   use numPrecision
-  use genericProcedures, only : fatalError
+  use errors_mod,        only : fatalError
   use dictionary_class,  only : dictionary
 
   ! source interface
@@ -11,6 +11,7 @@ module sourceFactory_func
   use pointSource_class,    only : pointSource
   use fissionSource_class,  only : fissionSource
   use materialSource_class, only : materialSource
+  use mixSource_class,      only : mixSource, mixSource_TptrCast
 
   ! geometry
   use geometry_inter,    only : geometry
@@ -24,9 +25,10 @@ module sourceFactory_func
   ! It is printed if type was unrecognised
   ! NOTE:
   ! For now  it is necessary to adjust trailing blanks so all entries have the same length
-  character(nameLen),dimension(*),parameter :: AVAILABLE_sources = [ 'pointSource   ',&
-                                                                     'fissionSource ',&
-                                                                     'materialSource']
+  character(nameLen), dimension(*), parameter :: AVAILABLE_sources = [ 'pointSource   ',&
+                                                                       'fissionSource ',&
+                                                                       'materialSource',&
+                                                                       'mixSource     ']
 
 contains
 
@@ -34,15 +36,18 @@ contains
   !! Allocate new allocatable source to a specific type
   !! If new is allocated it deallocates it
   !!
-  subroutine new_source(new, dict, geom)
-    class(source),allocatable, intent(inout) :: new
-    class(dictionary), intent(in)            :: dict
-    class(geometry), pointer, intent(in)     :: geom
-    character(nameLen)                       :: type
+  recursive subroutine new_source(new, dict, geom)
+    class(source), allocatable, intent(inout) :: new
+    class(dictionary), intent(in)             :: dict
+    class(geometry), pointer, intent(in)      :: geom
+    character(nameLen)                        :: type
+    integer(shortInt)                         :: i
+    type(mixSource), pointer                  :: mixSource_ptr
+    character(nameLen), dimension(:), allocatable :: sourceNames
     character(100),parameter :: Here = 'new_source (sourceFactory_func.f90)'
 
     ! Deallocate new if allocated
-    if(allocated(new)) deallocate(new)
+    if (allocated(new)) deallocate(new)
 
     ! Obtain string that specifies type to be built
     call dict % get(type,'type')
@@ -57,6 +62,21 @@ contains
 
       case('materialSource')
         allocate(materialSource :: new)
+
+      case('mixSource')
+        allocate(mixSource :: new)
+
+        mixSource_ptr => mixSource_TptrCast(new)
+        if (.not. associated(mixSource_ptr)) call fatalError(Here,'Failed to get mixSource pointer')
+
+        ! Build mixture of sources here
+        ! This is done to avoid circular dependences with mixSource_class
+        call dict % get(sourceNames, 'sources')
+        allocate(mixSource_ptr % sources(size(sourceNames)))
+
+        do i = 1, size(sourceNames)
+          call new_source(mixSource_ptr % sources(i) % sourceItem, dict % getDictPtr(sourceNames(i)), geom)
+        end do
 
      case default
        print *, AVAILABLE_sources
