6e154560d080a0fdc4fc986c47ed1f9f22c3ee34
ChasingNeutrons <pmc55@cam.ac.uk>
2025-12-13 17:26:19 +0000

    Unbreaking test

--- Geometry/Fields/VectorFields/Tests/uniFissSitesField_test.f90
+++ Geometry/Fields/VectorFields/Tests/uniFissSitesField_test.f90
@@ -84,8 +84,6 @@ contains
     EXPECTED_BINS = [0.25, 0.25, 0.0]
     bins = this % ufsField % atP(p)
     @assertEqual(EXPECTED_BINS, bins, tolerance=1.0e-6)
-    bins = this % ufsField % at(p % coords)
-    @assertEqual(EXPECTED_BINS, bins, tolerance=1.0e-6)
 
     ! Test case outside the map
     p % coords % lvl(1) % r = [0.5, 7.0, 100.0]
